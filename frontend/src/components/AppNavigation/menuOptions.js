a15379ad0f6fe801712a4f383308f0b4f06c89a0
chrisrappa <rappaproductionsinc@gmail.com>
2024-06-24 22:15:00 -0500

    added folders nav

--- frontend/src/components/AppNavigation/menuOptions.js
+++ frontend/src/components/AppNavigation/menuOptions.js
@@ -1,7 +1,9 @@
 import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import FolderIcon from '@mui/icons-material/Folder';
 
 export const navigationOptions = [
   {id: 1, name: "AI Chat", link: '/app-home', icon: <AutoAwesomeIcon sx={{fontSize: '1.5rem', color: '#C6C6C6'}} />}, 
+  {id: 2, name: "Folders", link: '/folders', icon: <FolderIcon sx={{fontSize: '1.5rem', color: '#C6C6C6'}} />}, 
   {id: 10, name: "Support", link: '/support', icon: <ContactSupportIcon sx={{fontSize: '1.5rem', color: '#C6C6C6'}} /> }
 ];
